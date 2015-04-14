dd449b1d39a0fe7eb1785ca9fc164e4f6f08b539
Ramaseshan Parthasarathy <tilluboy@gmail.com>
2015-04-13 22:54:09 -0400

    Face
    Face and MyFace; toy around with expressions "happy", "sad", "mad", etc.

---
+++ TheGroupProjects/src/thing/MyFaceApp.java
@@ -0,0 +1,16 @@
+package thing;
+
+public class MyFaceApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//MyFace face1 = new MyFace(500,500);
+        MyFace face2 = new MyFace(); // no-argument constructor
+		face2.setExpression("happy");
+
+	}
+
+}
